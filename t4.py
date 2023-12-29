ad55124f8fb98ce4c8f7a19d52f9c172b260ccb2
SyedNawaz786 <93913149+SyedNawaz786@users.noreply.github.com>
2023-12-29 09:55:35 +0000

    done all 6

---
+++ t4.py
@@ -0,0 +1,6 @@
+words = ["apple", "banana", "orange"]
+
+# Task: Use Copilot to complete the code to print each word in uppercase
+for word in words:
+    # TODO: Complete the code here
+    print(word.upper())
---
+++ t5.py
@@ -0,0 +1,8 @@
+numbers = [5, 2, 8, 1, 7]
+
+# Task: Use Copilot to complete the code to sort the array in ascending order
+# TODO: Complete the code here
+numbers.sort()
+
+# Print the sorted array
+print("Sorted Array in Ascending Order:", numbers)
